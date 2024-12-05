bd02ab64b0b005c61ea03a2b1fd682ecb61375f1
achyut vardhan <achyutvardhan234@gmail.com>
2024-12-05 16:41:25 +0530

    captain service done

--- gateway/app.js
+++ gateway/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use('/user' , proxy('http://localhost:3001'));
+app.use('/captain' , proxy('http://localhost:3002'));
+
 
 app.listen(3000 , ()=>{
     console.log("gateway is running on port 3000");
